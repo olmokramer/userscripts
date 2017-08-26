b0d0e0e65e394e2977e984bfacb5b3b8c2b51635
Olmo Kramer <olmo.kramer@gmail.com>
2017-08-26 10:50:05 +0200

    Reddit header background

--- reddit-layout.user.js
+++ reddit-layout.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://github.com/olmokramer
 // @description Nicer reddit layout
 // @include     https://*.reddit.com/*
-// @version     1
+// @version     2
 // @author      Olmo Kramer
 // @grant       none
 // ==/UserScript==
@@ -12,7 +12,10 @@
     'use strict';
 
     const style = document.createElement('style');
-    style.innerHTML = 'body { max-width: 1200px; margin-left: auto; margin-right: auto; }';
+    style.innerHTML = `
+    body { max-width: 1200px; margin-left: auto; margin-right: auto; }
+    #header { background-color: white; }
+    `;
     document.head.append(style);
 })();
 
