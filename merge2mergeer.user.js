add63cc492527ee46149badd8397bd3e7115e0d7
Olmo Kramer <olmo.kramer@gmail.com>
2018-02-14 21:45:41 +0100

    merge2mergeer: Don't replace code, just regular text

--- merge2mergeer.user.js
+++ merge2mergeer.user.js
@@ -10,51 +10,43 @@
 
 (function() {
 	const replacements = {
-		mergeable:  'mergeerbaar',
-		mergable:   'mergeerbaar',
-		merge:      'mergeer',
-		merging:    'mergeren',
-		push:       'druk',
-		pull:       'trek',
-		PR:         'TR',
-		commit:     'pleging',
-		committing: 'plegen',
-		rebase:     'herbaas',
-		rebasing:   'herbazen',
+		mergeable: 'mergeerbaar',
+		mergable:  'mergeerbaar',
+		merge:     'mergeer',
+		merging:   'mergeren',
+		push:      'druk',
+		pull:      'trek',
+		PR:        'TR',
 	};
+
+	function capitalise(str) {
+		return str[0].toUpperCase() + str.slice(1);
+	}
 
 	Object.keys(replacements).forEach(function(key) {
 		replacements[capitalise(key)] = capitalise(replacements[key]);
 	});
 
-	const regex = /merge?able|merg(e|ing)|push|pull|commit(ting)?|rebas(e|ing)|\bpr\b/gi
+	const regex = new RegExp(Object.keys(replacements).join('|'), 'g');
 
 	function merge2mergeer() {
-		const texts = [];
-		const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+		const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
+			acceptNode: function(node) {
+				if (!node.mergeer && !node.parentNode.closest('.blob-code')) {
+					return NodeFilter.FILTER_ACCEPT;
+				}
+			},
+		});
+
 		let node;
 		while ((node = walker.nextNode())) {
-			if (!node.mergeer) {
-				texts.push(node);
-			}
-		}
-
-		texts.forEach(function(text) {
-			text.textContent = text.textContent.replace(regex, function(match, idx) {
+			node.mergeer = true;
+			node.textContent = node.textContent.replace(regex, function(match, idx) {
 				return replacements[match] || match;
 			});
-			text.mergeer = true;
-		});
+		}
 	}
 
 	merge2mergeer();
 	setInterval(merge2mergeer, 2000);
-
-	function capitalise(str) {
-		let i = 0;
-		while (str[i] == '\\') {
-			i += 2;
-		}
-		return str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
-	}
 })();
